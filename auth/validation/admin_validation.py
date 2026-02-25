4af75a11a405c04217eb8523ae7ca631f55ea5d4
Wei1155 <xingweia05@gmail.com>
2026-02-25 20:47:46 +0800

    deleted

--- auth/validation/admin_validation.py
+++
@@ -1,7 +0,0 @@
-ADMIN_SECRET_KEY = "ELEARN2026"   # Must match register.py
-
-def validate_admin_key(key):
-    """
-    Check if admin secret key is correct.
-    """
-    return key == ADMIN_SECRET_KEY
